bd63408cf798b230051277a34e6d86501c057c4e
swillrodt <sumnerwillrodt@gmail.com>
2021-11-13 14:48:21 -0800

    add forms front-end logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,7 +43,9 @@ router.get('/', (req, res) => {
 });
 
 //Login
-
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 // router.get('/', (req, res) => {
 //   res.render('homepage');
