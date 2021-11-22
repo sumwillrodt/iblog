35d765fc85d8b3aaa102305cd25f779130896a24
swillrodt <sumnerwillrodt@gmail.com>
2021-11-21 19:13:08 -0800

    update post functions

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -41,4 +41,52 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPosts => {
+    if (!dbPosts) {
+      res.status(404).json({
+        message: 'No post found with this id'
+      });
+      return;
+    }
+
+    const post = dbPosts.get({
+      plain: true
+    });
+
+    res.render('edit-post', {
+      post,
+      loggedIn: req.session.loggedIn
+    });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
 module.exports = router;
